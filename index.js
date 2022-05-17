a6f60ce0a6640f6c92cbf2446ef269f8524989ae
Alberto Campos <albertocamposhdez@hotmail.com>
2022-05-16 19:30:39 -0500

    feat: add new function to check a proccess

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ function doSomething() {
 function functionFromNewBranch() {
   console.log("Hello World from new branch");
 }
+function checkProcess() {
+  console.log("Hello World from checkProcess");
+}
