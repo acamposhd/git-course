ab9aa4d5141245ede9ef8d5757b0570ae7c9f18f
Alberto Campos <albertocamposhdez@hotmail.com>
2022-05-16 19:42:06 -0500

    fix: fixed function doSomething

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function doSomething() {
-  console.log("Hello World");
+  console.log("Hello World solved bug in production");
 }
 function functionFromNewBranch() {
   console.log("Hello World from new branch");
